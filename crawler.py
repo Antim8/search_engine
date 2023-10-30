6b844e9e1e0730893bf1ea5a8e559d98d782ce2d
Antim8 <petersen.tim@outlook.de>
2023-10-30 19:34:56 +0100

    generated file works for part 1 of task

--- crawler.py
+++ crawler.py
@@ -1,8 +1,31 @@
+import requests
+from bs4 import BeautifulSoup
 
 
-def search():
-    pass
+def crawl(start_url):
+    index = []
+    r = requests.get(start_url)
+    # print(r.status_code)
+    # print(r.headers)
+    soup = BeautifulSoup(r.content, 'html.parser')
+    # print(soup.title.text)
+    # print(soup.text)
+    for l in soup.find_all("a"):
+        print(l['href'])
+        print(l.text)
 
+
+
+def search(words):
+    result = []
+    for word in words:
+        if word in index:
+            result.append(index[word])
+    return result
 
 if __name__ == "__main__":
-    pass
+    start_url = "https://vm009.rz.uos.de/crawl/index.html"
+    index = crawl(start_url)
+    search_words = ["platypus", "fossil"]
+    search_result = search(search_words)
+    print(search_result)
